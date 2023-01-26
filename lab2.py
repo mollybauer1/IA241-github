9180e6ef5cf7838da62755b4bc4d21e7b3a6ec23
EC2 Default User <ec2-user@ip-172-31-48-197.ec2.internal>
2023-01-26 20:56:09 +0000

    my lab2.py file.

---
+++ lab2.py
@@ -0,0 +1,25 @@
+"""
+lab 2
+"""
+#6.1
+my_name = "Tom"
+print(my_name.upper())
+#6.3
+my_id = 123
+print(my_id)
+#6.4
+my_id=your_id=123
+#6.5 
+my_id_str = '123'
+print(my_id_str)
+#6.6
+print(my_name+my_id_str)
+#6.7
+print(my_name+my_id_str)
+#6.8
+print(my_name*3)
+#6.9
+print("hello,worl. This is my first python string.".split("."))
+#6.10
+message = "Tom's id is 123."
+
---
+++ lec2.py
@@ -0,0 +1,16 @@
+"""
+lec 2
+numbers, string, and variables
+"""
+print(type('hello')) #single line comment
+print(type(123))
+print(type(12.3))
+
+nums = (1,2,3)
+print(type(nums))
+
+def molly(i):
+    return i
+}
+
+
